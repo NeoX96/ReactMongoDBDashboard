9c5c3daeeb4bbedc47d19cf3d913843365207853
Valentin <valentin@wuerfelmail.de>
2023-02-27 02:30:15 +0100

    added documentation

--- src/data/shopsData_live.js
+++ src/data/shopsData_live.js
@@ -1,17 +1,70 @@
 import * as Realm from "realm-web";
 
-const app = new Realm.App({ id: "data-ngpoq" });
-const credentials = Realm.Credentials.apiKey("2Xyb6zk9mkSNb1knmcAEdX1GCahmGhyi3YMFM8qo9YW86hoVEigf2GGTe8iqNpG8");
+const APP_ID = "data-uilqj";
+const API_KEY = "7PodJEFumzYtvIBgsWkiBMp5N1ifzHkrRMO9XTvRSxVBH5TIq5kJdXhLxE7RKWKM";
+const DATA_SOURCE = "DBDatabase";
+const DATABASE_NAME = "Top10";
+const COLLECTION_NAME = "10Shops";
 
 
-export async function getMongoCollection() {
-  const user = await app.logIn(credentials);
-  const mongo = user.mongoClient("DBDatabase");
-  const collection = mongo.db("Top10").collection("10Shops");
+const app = new Realm.App(APP_ID);
+const credentials = Realm.Credentials.apiKey(API_KEY);
+
+/**
+ * Loggt einen Benutzer bei der Realm-Instanz ein.
+ *
+ * @returns {object} Der eingeloggte Benutzer.
+ */
+async function realmLogin() {
+  try {
+    // Überprüfe, ob app definiert ist
+    if (!app) {
+      console.error("app is not defined");
+      return;
+    }
+
+    const user = await app.logIn(credentials);
+
+    if (user.id !== app.currentUser?.id) {
+      console.error("User is not logged in");
+    }
+
+    console.log("User logged in:", user);
+    return user;
+  } catch (err) {
+    console.error("Failed to log in", err);
+  }
+}
+
+
+async function getMongoCollection() {
+  const user = app.currentUser ?? await realmLogin();
+
+  const mongo = user.mongoClient(DATA_SOURCE);
+  const db = mongo.db(DATABASE_NAME);
+  if (!db) {
+    throw new Error(`Failed to connect to database ${DATABASE_NAME}`);
+  }
+  const collection = db.collection(COLLECTION_NAME);
+  console.log("Collection:", collection);
+  
   return collection;
 }
 
+
+/**
+ * Überwacht eine MongoDB-Sammlung auf Änderungen und führt eine angegebene Funktion aus, wenn eine Änderung auftritt.
+ *
+ * @param {function} onChange - Eine Funktion, die ausgeführt wird, wenn eine Änderung in der Sammlung auftritt. Die aktualisierten Daten werden als Argument an die Funktion übergeben.
+ * @returns {function} Eine Funktion, die den Änderungsstrom schließt.
+ */
 export async function watchCollection(onChange) {
+  try {
+    // Überprüfe, ob getMongoCollection definiert ist
+    if (typeof await getMongoCollection !== "function") {
+      console.error("getMongoCollection is not defined");
+      return;
+    }
 
     const collection = await getMongoCollection();
     const changeStream = await collection.watch();
@@ -24,30 +77,35 @@ export async function watchCollection(onChange) {
       console.log("Data changed:", updatedShop);
     }
 
-    return () => {
-      changeStream.close();
-    }
-}
+    console.log("Change stream closed");
+    changeStream.close();
 
-export async function realmLogin() {
-  try {
-    const user = await app.logIn(credentials);
-
-    if (user.id !== app.currentUser?.id) {
-      console.error("User is not logged in");
-    }
-
-    return user;
   } catch (err) {
-    console.error("Failed to log in", err);
+    console.error("Failed to watch collection", err);
   }
 }
 
 
-export async function getShopsData() {
 
+
+
+/**
+ * Gibt die Daten der MongoDB-Sammlung zurück.
+ *  
+ * @returns {array} Ein Array mit den Daten der MongoDB-Sammlung.
+ *  
+ */
+export async function getShopRevenuePieChart() {
   const collection = await getMongoCollection();
-  const shopsData = await collection.find({}, { limit: 10 }).toArray(); 
+  const data = await collection.find();
 
-  return shopsData;
+  const formattedData = data.map(({ ShopName, Revenue }) => ({
+    id: ShopName,
+    label: ShopName,
+    value: Revenue,
+  }));
+
+  console.log("Data:", formattedData);
+
+  return formattedData;
 }
