70bb138342cab8e60df76b859b45c89a80c05265
Valentin <valentin@wuerfelmail.de>
2023-02-27 20:42:28 +0100

    added fallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
@@ -9,6 +9,7 @@ import Pie from "./scenes/pie";
 import Data from "./scenes/funnel";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -22,14 +23,16 @@ function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/data" element={<Data />} />
-              <Route path="*" element={<h1>404</h1>} />
-            </Routes>
+            <Suspense fallback={<CircularProgress />}>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/data" element={<Data />} />
+                <Route path="*" element={<h1>404</h1>} />
+              </Routes>
+            </Suspense>
           </main>
         </div>
       </ThemeProvider>
