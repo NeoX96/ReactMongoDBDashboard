eb41317619d5c8bb2910f885befb8b0d1fc174aa
Valentin <valentin@wuerfelmail.de>
2023-02-28 16:09:41 +0100

    changed background color

--- src/theme.js
+++ src/theme.js
@@ -159,7 +159,7 @@ export const themeSettings = (mode) => {
               light: colors.grey[100],
             },
             background: {
-              default: "#fbfdf6",
+              default: "#FAF9F6",
             },
           }),
     },
